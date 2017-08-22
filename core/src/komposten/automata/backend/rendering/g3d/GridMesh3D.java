951cf3c5025277010c08260942b2bad1e777867d
Komposten <j.hjelm586@hotmail.com>
2017-08-22 20:01:51 +0200

    GridMesh3D no longer creates vertices for "hidden" cell faces
    - GridMesh3D now hides all Cell faces that are put against each other (visible neighbouring blocks).
    - GridMesh3D now uses a FloatArray object to store its vertices, to facilitate future removal and addition of vertices.

--- core/src/komposten/automata/backend/rendering/g3d/GridMesh3D.java
+++ core/src/komposten/automata/backend/rendering/g3d/GridMesh3D.java
@@ -4,8 +4,10 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Mesh;
 import com.badlogic.gdx.math.RandomXS128;
 import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.FloatArray;
 
 import komposten.automata.backend.rendering.Vertex;
+import komposten.automata.backend.rendering.g3d.Cell3D.Face;
 
 
 public class GridMesh3D implements Disposable
@@ -13,6 +15,7 @@ public class GridMesh3D implements Disposable
 	private Mesh mesh;
 	private Cell3D[] cells;
 	private float[] vertexArray;
+	private FloatArray floatArray;
 	
 	private int cellCount;
 	private int width; //X
@@ -69,6 +72,8 @@ public class GridMesh3D implements Disposable
 					Cell3D cell = new Cell3D(xPos, yPos, zPos, cellSize, colour);
 					
 					cells[index] = cell;
+					
+					updateCell(index, x, y, z);
 				}
 			}
 		}
@@ -90,37 +95,47 @@ public class GridMesh3D implements Disposable
 		if (mesh == null)
 			mesh = new Mesh(false, true, getCellCount() * Cell3D.VERTICES_PER_CELL, 0, Vertex.VERTEX_ATTRIBUTES);
 		
+		vertexArray = floatArray.toArray();
 		mesh.setVertices(vertexArray);
 		
 		
-//		System.out.println("Vertex objects: " + (cells.length * Cell3D.VERTICES_PER_CELL) + ", floats: " + vertexArray.length + ", in mesh: " + mesh.getNumVertices());
+		System.out.println("Vertex objects: " + (cells.length * Cell3D.VERTICES_PER_CELL) + ", floats: " + vertexArray.length + ", in mesh: " + mesh.getNumVertices());
 	}
 	
 	
 	private void createVertexArray()
 	{
 		int valuesPerCell = Cell3D.VERTICES_PER_CELL * Vertex.VALUES_PER_VERTEX;
-		vertexArray = new float[cells.length * valuesPerCell];
+		int maximumValues = cells.length * valuesPerCell;
+		
+		floatArray = new FloatArray(maximumValues);
 		
 		for (int i = 0; i < cells.length; i++)
 		{
 			Cell3D cell = cells[i];
+			
+			if (!cell.isVisible())
+				continue;
+			
 			int index = i * valuesPerCell;
 			
 			for (int j = 0; j < Cell3D.VERTICES_PER_CELL; j++)
 			{
+				if (!cell.isVertexVisible(j))
+					continue;
+				
 				int k = index + j*Vertex.VALUES_PER_VERTEX;
 				Vertex vertex = cell.getVertices()[j];
 				
-				vertexArray[k+0] = vertex.x;
-				vertexArray[k+1] = vertex.y;
-				vertexArray[k+2] = vertex.z;
-				vertexArray[k+3] = vertex.u;
-				vertexArray[k+4] = vertex.v;
-				vertexArray[k+5] = vertex.r;
-				vertexArray[k+6] = vertex.g;
-				vertexArray[k+7] = vertex.b;
-				vertexArray[k+8] = vertex.a;
+				floatArray.add(vertex.x);
+				floatArray.add(vertex.y);
+				floatArray.add(vertex.z);
+				floatArray.add(vertex.u);
+				floatArray.add(vertex.v);
+				floatArray.add(vertex.r);
+				floatArray.add(vertex.g);
+				floatArray.add(vertex.b);
+				floatArray.add(vertex.a);
 			}
 		}
 	}
@@ -175,9 +190,108 @@ public class GridMesh3D implements Disposable
 	}
 	
 	
+	public void updateCell(Color color, boolean visible, int index)
+	{
+		cells[index].setColor(color);
+		cells[index].setVisible(visible);
+		updateCell(index);
+	}
+	
+	
+	public void updateCell(Color color, boolean visible, int x, int y, int z)
+	{
+		int index = getIndex(x, y, z);
+		cells[index].setColor(color);
+		cells[index].setVisible(visible);
+		
+		updateCell(index, x, y, z);
+	}
+	
+	
+	public void updateCell(int index)
+	{
+		//TODO call updateCell(int, int, int)
+	}
+	
+	
+	public void updateCell(int index, int x, int y, int z)
+	{
+		Cell3D cell = cells[index];
+		Cell3D adjacent = null;
+		
+		//Right neighbour
+		if (x+1 < width)
+		{
+			adjacent = cells[getIndex(x+1, y, z)];
+			if (adjacent != null)
+			{
+				adjacent.setFaceVisible(Face.Left, !cell.isVisible());
+				cell.setFaceVisible(Face.Right, !cell.isVisible());
+			}
+		}
+		
+		//Left neighbour
+		if (x-1 >= 0)
+		{
+			adjacent = cells[getIndex(x-1, y, z)];
+			if (adjacent != null)
+			{
+				adjacent.setFaceVisible(Face.Right, !cell.isVisible());
+				cell.setFaceVisible(Face.Left, !cell.isVisible());
+			}
+		}
+		
+		//Front neighbour
+		if (z+1 < depth)
+		{
+			adjacent = cells[getIndex(x, y, z+1)];
+			if (adjacent != null)
+			{
+				adjacent.setFaceVisible(Face.Back, !cell.isVisible());
+				cell.setFaceVisible(Face.Front, !cell.isVisible());
+			}
+		}
+		
+		//Back neighbour
+		if (z-1 >= 0)
+		{
+			adjacent = cells[getIndex(x, y, z-1)];
+			if (adjacent != null)
+			{
+				adjacent.setFaceVisible(Face.Front, !cell.isVisible());
+				cell.setFaceVisible(Face.Back, !cell.isVisible());
+			}
+		}
+		
+		//Top neighbour
+		if (y+1 < height)
+		{
+			adjacent = cells[getIndex(x, y+1, z)];
+			if (adjacent != null)
+			{
+				adjacent.setFaceVisible(Face.Bottom, !cell.isVisible());
+				cell.setFaceVisible(Face.Top, !cell.isVisible());
+			}
+		}
+		
+		//Bottom neighbour
+		if (y-1 >= 0)
+		{
+			adjacent = cells[getIndex(x, y-1, z)];
+			if (adjacent != null)
+			{
+				adjacent.setFaceVisible(Face.Top, !cell.isVisible());
+				cell.setFaceVisible(Face.Bottom, !cell.isVisible());
+			}
+		}
+	}
+	
+	
 	public void refreshMesh()
 	{
 		createVertexArray();
+//		updateFloatArray();
+		vertexArray = floatArray.toArray();
 		mesh.updateVertices(0, vertexArray);
 	}
 	
